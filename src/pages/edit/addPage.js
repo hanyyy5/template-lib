4ba9aea01f8bd66c0a2c57b68bf6e4ffa945fd9a
yuhan <hanyustu@163.com>
2020-07-04 18:11:49 +0800

    编辑页

--- src/pages/edit/addPage.js
+++ src/pages/edit/addPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Form, Input } from 'antd';
 import { SettingFilled,  SyncOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 
+const { Search } = Input;
 const addPage = (props) => {
   const [form] = Form.useForm();
 
@@ -12,14 +14,13 @@ const addPage = (props) => {
   return (
       <div className="add-page">
            <Form
-                style={{ paddingTop: '100px' }}
                 name="basic"
                 layout={'vertical'}
                 form={form}
                 onValuesChange={onFormLayoutChange}
             >
                 <Form.Item label="1.填写页面名称" name="proName" rules={[{ required: true, message: '请输入页面名称!' }]}>
-                <Input placeholder="请输入页面名称" />
+                <Input placeholder="名称必须英文字母A-Z开头，只包含英文和数字，不允许有特殊字符" />
                 </Form.Item>
             </Form>
             <div className="choose-block">
@@ -27,6 +28,7 @@ const addPage = (props) => {
                 <div className="blocks-content">
                      <div className="block-settle">
                           <div className="defaulte">请从右侧选择区块</div>
+
                      </div>
                      <div className="block-collect">
                           <div className="block-title">
@@ -34,9 +36,18 @@ const addPage = (props) => {
                                <SettingFilled />
                                <SyncOutlined />
                           </div>
+                          <Search
+                            placeholder="输入关键字查找物料"
+                            onSearch={value => console.log(value)}
+                            style={{ width: 200 }}
+                          />
                      </div>
                 </div>
             </div>
+            <div className="button-line">
+              <Button type="primary"> 生成页面 </Button>
+            </div>
+
       </div>
       
   );
--- src/pages/edit/index.js
+++ src/pages/edit/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import AddPage from './addPage';
 import "./index.scss";
 
 class Edit extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+
     };
   }
 
@@ -14,14 +16,19 @@ class Edit extends React.Component {
     return (
         <div className="edit-page">
              <div className="left-file-tree">
-
+                 <div className="first-module">
+                      
+                 </div>
+                 <div className="second-module">
+                      
+                 </div>
              </div>
              <div className="right-content">
                   <div className="tab">
                        
                   </div>
                   <div className="tab-content">
-                        
+                       <AddPage /> 
                   </div>
              </div>
         </div>
